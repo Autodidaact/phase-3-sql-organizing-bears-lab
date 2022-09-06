6e18086afbe746051f56c462baf008f75e0558ba
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-09-06 16:35:03 +0000

    Modified create.sql, insert.sql && sql_queries.rb

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Mr. Chocolate", 8, "M", "black", "melancholic", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Rowdy", 6, "F", "white", "sanguin", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Tabitha", 15, "M", "brown", "consentious", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Sergeant Brown", 10, "M", "brown", "phelamatic", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Melissa", 12, "F", "black", "melancholic", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Grinch", 5, "F", "white", "melancholic", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Wendy", 7, "F", "brown", "sanguin", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(NULL, NULL, "F", "brown", NULL, 0);
+
+
+
+
